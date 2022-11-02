1c80469d172b69878818bfea22bda7688050af9f
Murilo JF <115420695+Onuzim98@users.noreply.github.com>
2022-11-02 12:29:09 -0300

    Add files via upload

--- Boxer-Pets-main/feed.js
+++ Boxer-Pets-main/feed.js
@@ -4,8 +4,16 @@ const menuItems = document.querySelectorAll('.menu-item');
 
 //mensagens
 
-const messagesnotification = document.querySelector('#messagse-notification');
+const messagesnotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.messages');
+const messageSearch = document.querySelector('#message-search');
+
+// Thema
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector ('.customize-theme');
+const fontSizes = document.querySelectorAll ('.choose-size');
+var root = document.querySelector(':root');
 
 //remove active class from all menu items
 const changeActiveItem = () => {
@@ -19,18 +27,53 @@ menuItems.forEach(item => {
         changeActiveItem();
         item.classList.add('active');
         if(item.id != 'notifications'){
-            document.querySelector('.notification-popup').style.display = 'none';
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
 
         } else {
-            document.querySelector('.notifications-popup').style.display = 'block';
-            document.querySelector('#notifications .notification-count').style.display = 'none';
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('#notifications .notification-count')
+            .style.display = 'none';
         }
     })
 })
 
 
 //messages
+//buscar chats
 
+
+//Barra de pesquisa
+
+
+//mensagem com luizinha
 messagesnotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesnotification.querySelector('.notification-count').style.display =
+    'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000)
 })
+
+
+//Temas arg
+
+//open modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid' ;
+}
+
+//fechar modal part 2
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none'; 
+    }
+}
+
+
+//fecha modal
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click',  openThemeModal);
